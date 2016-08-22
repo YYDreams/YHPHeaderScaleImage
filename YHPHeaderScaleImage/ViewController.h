b3ba470dcb5a9fa572b42ebde565449e7eed02a1
YYDreams <799431668@qq.com>
2016-08-22 17:57:17 +0800

    Initial Commit

---
+++ YHPHeaderScaleImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHPHeaderScaleImage
+//
+//  Created by LOVE on 16/8/22.
+//  Copyright © 2016年 LOVE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
